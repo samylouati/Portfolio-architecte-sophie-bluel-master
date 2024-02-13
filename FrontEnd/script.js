d9c578e7d85fc4bb5e1db5fde2c49fdf0f1acf81
samylouati <148778977+samylouati@users.noreply.github.com>
2024-02-13 13:10:50 +0100

    displayworks ok

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,6 +1,7 @@
 console.log('yo') // pour verfifier si mon console.log fonctionne (f12) sur le navigateur
 
 const API_BASE_URL = "http://localhost:5678/api"; // je crée cette variable pour l'url de base de l'API
+console.log(API_BASE_URL)
 
 // pour recuperer les WORKS, je fais un fetch :
 // fetch (`URL de l'api à appeler`) // si j'arrive à communiquer avec l'API
@@ -15,56 +16,27 @@ getApiWorks(); // appel de la function à l'ouverture de la page
 function getApiWorks () {
     fetch (`${API_BASE_URL}/works`) // je recupere les travaux de l'architecte sur l'API
     .then ((response) => response.json())
-    .then ((data) => {
-        console.log(data);
-        DisplayWorks();
+    .then (projets => {
+        console.log(projets);
+        DisplayWorks(projets); // j"appelle ma fonction DisplayWorks
     })
     .catch ((error) => {console.error(error)
     })
 }
 
-// je crée ce qui va me servir à afficher les images du portfolio, fonction DisplayWorks
-const article = API_BASE_URL[0];
-const gallery = document.querySelector(".gallery"); // je recupere la gallery
-
-// const figure = document.createElement("figure"); // je crée une balise <figure>
-// figure.classList.add('figure'); // j'applique le style 'figure' sur la balise <figure>
-
-// const images = document.createElement("img"); // je crée une balise <img>
-// images.src = article.imageUrl; // je veux que la source soit "imageUrl" present dans le tableau 
-
-// const legend = document.createElement("figcaption"); // je crée une balise <figcaption>
-// legend.innerText = article.title; // je veux que le text de legend soit "title" present dans le tableau 
-
-// gallery.appendChild(figure); // gallery est parent de figure
-// figure.appendChild(images); // figure est parent de images
-// figure.appendChild(legend); // figure est parent de legend
-
 // Fonction pour montrer les elements du tableau
 
-function DisplayWorks () {
-    Projet.forEach(article) => {
+const gallery = document.querySelector(".gallery"); // je recupere la gallery dans le DOM
+
+function DisplayWorks (projets) {
+    projets.forEach(projetS => {
         const figure = document.createElement("figure"); // je crée une balise <figure> pour chaque projet
         figure.classList.add('figure'); // j'applique le style 'figure sur chaque balise <figure>
         const images = document.createElement("img");
-        images.src = imageUrl; // j'indique que le chemin des images est "imageUrl" dans le tableau
+        images.src = projetS.imageUrl; // j'indique que le chemin des images est "imageUrl" dans le tableau
         const legend = document.createElement("figcaption"); // je crée la balise <figcaption>
-        legend.innerText = title; // j'indique que le texte des balises <figcaption> est "title" dans le tableau
+        legend.innerText = projetS.title; // j'indique que le texte des balises <figcaption> est "title" dans le tableau
         gallery.appendChild(figure); // gallery est parent de chaque <figure>
         figure.appendChild(images); // figure est parent de images
-        figure.appendChild(legend); // figure est parent de legend
-    }
-
-
-}
-
-function updateDots () {
-	allDots.forEach((dot, Index) => {
-		if (Index === currentSlideIndex) {
-			dot_selected = dot.classList.add('dot_selected');
-		}
-		else {
-			dot_unselected = dot.classList.remove('dot_selected');
-		}
-	});
-}
+        figure.appendChild(legend); // figure est parent de legend    
+})};
