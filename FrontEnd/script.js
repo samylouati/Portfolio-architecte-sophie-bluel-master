5a70ebba1f28d8522676337052a05c4452d5c7c1
samylouati <148778977+samylouati@users.noreply.github.com>
2024-02-13 17:04:26 +0100

    premier test de filtre

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -11,25 +11,26 @@ console.log(API_BASE_URL)
 
 // Etape 1 du guide des étapes clés
 
-getApiWorks(); // appel de la function à l'ouverture de la page
-
 function getApiWorks () {
     fetch (`${API_BASE_URL}/works`) // je recupere les travaux de l'architecte sur l'API
-    .then ((response) => response.json())
+    .then ((response) => response.json()) // je veux ces travaux au format JSON
     .then (projets => {
-        console.log(projets);
+        console.log(projets); // je nome ces travaux "projets"
         DisplayWorks(projets); // j"appelle ma fonction DisplayWorks
+        getApiCategories(); // je recupere les categories apres avoir recuperer les travaux
     })
     .catch ((error) => {console.error(error)
     })
 }
 
+getApiWorks(); // appel de la function à l'ouverture de la page
+
+
 // Fonction pour montrer les elements du tableau
 
-const gallery = document.querySelector(".gallery"); // je recupere la gallery dans le DOM
-
 function DisplayWorks (projets) {
-    projets.forEach(projetS => {
+    const gallery = document.querySelector(".gallery"); // je recupere la gallery dans le DOM
+    projets.forEach(projetS => { // pour chaque projetS(ophie) : 
         const figure = document.createElement("figure"); // je crée une balise <figure> pour chaque projet
         figure.classList.add('figure'); // j'applique le style 'figure sur chaque balise <figure>
         const images = document.createElement("img");
@@ -40,3 +41,40 @@ function DisplayWorks (projets) {
         figure.appendChild(images); // figure est parent de images
         figure.appendChild(legend); // figure est parent de legend    
 })};
+
+// Etape 1.2 du guide des étapes clés
+
+// Fonction pour recuperer les categories
+
+function getApiCategories () {
+    fetch (`${API_BASE_URL}/categories`) // je recupere les catégories sur l'API
+    .then ((response) => response.json()) // je veux ces catégories au format JSON
+    .then (categories => {
+        console.log(categories); // je nome ces catégories
+        // DisplayCategories(categories);
+    })
+    .catch ((error) => {console.error(error)
+    })
+}
+
+// Fonction pour afficher les categories
+
+// function DisplayCategories (categories) { 
+//     const filter = document.getElementById("portfolio"); // je recupere l'id "portfolio" dans le DOM
+//     categories.forEach(categoriesS => { // pour chaque categorieS(ophie) : 
+//         const button = document.createElement("button"); // je crée un bouton pour chaque categories
+//         // class à mettre sur mes bouton de trie
+//         filter.appendChild(button); // gallery est parent de input
+//         button.innerText = categoriesS.name; // j'indique que le texte des balises <figcaption> est "title" dans le tableau
+//     })};
+
+// Filtrer avec mes boutons
+
+const TrieObjets = document.querySelector('objet');
+
+TrieObjets.addEventListener("click", function () {
+    const objet_test = projets.filter( function (projet) {
+        return projet.id = 1;
+    });
+    console.log(objet_test)
+});
