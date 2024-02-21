90fa7ad27a277d373d1f93e33bdb1b5442a1701e
samylouati <148778977+samylouati@users.noreply.github.com>
2024-02-21 17:04:58 +0100

    texte login

--- FrontEnd/login.js
+++ FrontEnd/login.js
@@ -2,9 +2,18 @@ console.log('yo') // pour verfifier si mon console.log fonctionne (f12) sur le n
 
 const API_BASE_URL = "http://localhost:5678/api"; // je crée cette variable pour l'url de base de l'API
 
-//Fonction pour recuperer les id login sur l'API
-function getApiUsers() {
-    fetch (`${API_BASE_URL}/userd/login`,
-    method: 'POST',
+const email = document.getElementById("#email"); // je recupere mon input email
+const password = document.getElementById("#password"); // je recupere mon imput pasword
+const button = document.getElementById("#button");// je  recupere l'input se connecter
 
- localStorage
+const TextEmail = "sophie.bluel@test.tld";
+const TextPassword = "S0phie";
+
+button.addEventListener("click", function() {
+    if (email === TextEmail || password === TextPassword) { // si mail et password ok
+        console.log(index.HTML) //ouvrir l'index.html
+    }
+    else { 
+        console.log("L'email et/ou le mot de passe sont incorrect!")
+    }
+});
